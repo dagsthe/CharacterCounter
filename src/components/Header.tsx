45eb57034a62e0f52e6d9411ad346de9914d3d0c
Eric Pham <daggerstudiollc@gmail.comm>
2025-04-06 16:42:05 -0500

    fixed logo refresh button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function(){
         <>
             <div className='mt-10 md:mb-10 lg:mb-20 relative'>
                 <div className='flex flex-row gap-5'>
-                    <img src={Logo} alt="logo" className='aspect-square w-auto max-w-[30px] md:max-w-[60px] h-auto dark:invert opacity-80 cursor-pointer' onClick={() => (window.location.href = "/")}></img>
+                    <img src={Logo} alt="logo" className='aspect-square w-auto max-w-[30px] md:max-w-[60px] h-auto dark:invert opacity-80 cursor-pointer' onClick={() => window.location.reload()}></img>
                     <h1 className="text-2x md:text-5xl font-bold w-auto font-mono opacity-80">Character Counter</h1>
                 </div>
                 
